959d2027cffc2bb4f11e244b08b11894d337b397
carlospph <80932010+carlospph@users.noreply.github.com>
2024-06-03 22:49:09 -0300

    Update eventos.js

--- Menu/eventos.js
+++ Menu/eventos.js
@@ -1,1 +1,7 @@
+function openMenuMobile() {
+  document.querySelector('.window-menu-mobile').style.top = '0';
+}
 
+function closeMenuMobile() {
+  document.querySelector('.window-menu-mobile').style.top = '-2000px';
+}
