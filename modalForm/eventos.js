8f2290bcfb9026fe7a62cf7f386f11f5aa01a5aa
carlospph <80932010+carlospph@users.noreply.github.com>
2024-06-06 18:40:24 -0300

    Create eventos.js

---
+++ modalForm/eventos.js
@@ -0,0 +1,9 @@
+function openModal(idModal){
+  windowModal = document.getElementById(idModal); 
+  windowModal.style.left = '0';
+}
+
+function closeModal(idModal){ 
+     windowModal.style.left = '-900px';
+
+}
